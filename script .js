92f51b2cd91b571a6dbc096135336e84cbd8a9f1
Khanak Korotania <khanakkoro@gmail.com>
2024-11-12 12:29:32 +0530

    script .js

--- script .js
+++ script .js
@@ -8,11 +8,21 @@ const typeEffect = () => {
     const currentWord = words[wordIndex];
     const currentChar = currentWord.substring(0, charIndex);
     dynamicText.textContent = currentChar;
-    dynamicText.classList.add("stop-blinking");
-
+    
     if (!isDeleting && charIndex < currentWord.length) {
         charIndex++;
         setTimeout(typeEffect, 200);
-    } 
-}
+    } else if (isDeleting && charIndex > 0) {
+        charIndex--;
+        setTimeout(typeEffect, 100);
+    } else if (!isDeleting && charIndex === currentWord.length) {
+        isDeleting = true;
+        setTimeout(typeEffect, 1000); // pause before deleting
+    } else if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        wordIndex = (wordIndex + 1) % words.length; // loop back to first word
+        setTimeout(typeEffect, 500);
+    }
+};
+
 typeEffect();
